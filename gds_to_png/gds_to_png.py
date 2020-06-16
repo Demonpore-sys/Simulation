added per-group text area, with work-in-progress overlap calculation (I just found and fixed what I think was a bug with cropping the slit-layer, but using the offset where the pores should end up). There's still a bug, the overlap is always non-zero, so I'm doing something wrong...)

gds_to_png/gds_to_png.py
@@ -13,7 +13,6 @@ l2_gds = core.GdsImport(os.path.abspath("die5_from_topleft_layer2_shown.GDS"), v
 print('imported GDS units {} precision {}'.format(l4_gds.unit, l4_gds.precision))
 tl_4 = l4_gds.top_level()
 l4 = tl_4[0]
-
 tl_2 = l2_gds.top_level()
 l2 = tl_2[0]
 
@@ -25,11 +24,16 @@ l2mins, l2maxs = l2.bounding_box
 l2w = l2maxs[0] - l2mins[0]
 l2h = l2maxs[1] - l2mins[1]
 
-int_min_x = int(min(l4mins[0], l2mins[0]))
-int_min_y = int(min(l4mins[1], l2mins[1]))
+# how many decimal places to keep... 1 means none
+quality_factor = 1
 
-int_h = int(max(l2h, l4h))
-int_w = int(max(l2w, l4w))
+int_min_x = int(min(l4mins[0], l2mins[0])*quality_factor)
+int_min_y = int(min(l4mins[1], l2mins[1])*quality_factor)
+
+int_h = int(max(l2h, l4h)*quality_factor)
+int_w = int(max(l2w, l4w)*quality_factor)
+
+print('found bounding box for all pores on this die')
 
 
 def add_slits(image):
@@ -41,9 +45,8 @@ def add_slits(image):
         x2, y2 = corner2
         img1.rectangle(((x1, y1), (x2, y2)), fill=0, outline=0)
 
-pore_group_bounds = []
-pore_points_x = []
-pore_points_y = []
+# pore_points_x = []
+# pore_points_y = []
 pore_points = []
 
 
@@ -51,44 +54,52 @@ def add_pores(image, off_x=0, off_y=0, collect_points=False):
     for obj in l2.elements:
         for coord in obj.points:
             x, y = coord
-            xx = int(x - int_min_x)
-            yy = int(y - int_min_y)
+            xx = int((x*quality_factor) - int_min_x)
+            yy = int((y*quality_factor) - int_min_y)
             try:
                 image.putpixel((xx+off_x, yy+off_y), 0)
                 if collect_points:
-                    pore_points_x.append(xx)
-                    pore_points_y.append(yy)
+                    # pore_points_x.append(xx)
+                    # pore_points_y.append(yy)
                     pore_points.append((xx, yy))
             except IndexError as e:
                 print('{} {} w {} h {}'.format(xx, yy, int_w, int_h))
                 raise e
 
 
+print('adding slits to new image')
 l4_output = Image.new("1", (int_w + 1, int_h + 1), color=1)
 add_slits(l4_output)
 l4_output.save('l4_slits.png')
+print('saved slits to new image')
 #l4_mask = ImageMath.eval('1-(a)', a=l4_output).convert('1', dither=None)
 #l4_mask = 1 - numpy.asarray(l4_output)
 l4_mask = ImageOps.invert(l4_output.convert('L', dither=None)).convert('1', dither=None)
 l4_mask.save('l4_mask.png')
+print('saved slits mask image')
 
+print('adding pores to new image')
 l2_output = Image.new("1", (int_w + 1, int_h + 1), color=1)
 add_pores(l2_output, collect_points=True)
 l2_output.save('l2_pores.png')
+print('saved pores to new image')
 l2_mask = ImageOps.invert(l2_output.convert('L', dither=None)).convert('1', dither=None)
-
-
+print('created pores mask image')
+print('pasting slits + pores into new image')
 combined = Image.new("1", (int_w + 1, int_h + 1), color=1)
-combined.paste(l2_output, (0,0), l2_mask)
-combined.paste(l4_output, (0,0), l4_mask)
+combined.paste(l2_output, (0, 0), l2_mask)
+combined.paste(l4_output, (0, 0), l4_mask)
 combined.save("l2_l4.png")
-
+print('saved pores+slits combination image')
 
 points_sorted_by_x = sorted(pore_points, key=lambda x: x[0])
 points_sorted_by_y = sorted(pore_points, key=lambda x: x[1])
+print('sorted pore points for binning')
+
+
 def do_binning():
     x_bins = []
-    y_bins = []
+    final_bins = []
     o, bin_edges_x = np.histogram([x for x, y in points_sorted_by_x], bins=4)
     o, bin_edges_y = np.histogram([y for x, y in points_sorted_by_y], bins=4)
     #occurence, bin_edges = np.histogram([1, 2, 3], bins=16)
@@ -96,13 +107,16 @@ def do_binning():
         x_bins.append([])
         x_bins[-1] = [point for point in points_sorted_by_x if point[0] >= bin_edges_x[bin] and point[0] <= bin_edges_x[bin + 1]]
     for col in x_bins:
-        y_bins.append([])
-        for bin in range(4):
-            y_bins[-1].append([])
-            y_bins[-1][-1] = [point for point in col if point[1] >= bin_edges_y[bin] and point[1] <= bin_edges_y[bin + 1]]
-    return y_bins
+        y_bins = []
+        for row_num in range(4):
+            group_bin = [point for point in col if point[1] >= bin_edges_y[row_num] and point[1] <= bin_edges_y[row_num + 1]]
+            y_bins.append(group_bin)
+        final_bins.append(y_bins)
+    return final_bins
+
 
 subsets = do_binning()
+print('binned GDS points into dies/clusters')
 
 
 def find_largest_pore_group_size():
@@ -112,24 +126,58 @@ def find_largest_pore_group_size():
             xs = [x for x, y in row]
             ys = [y for x, y in row]
             minx = min(xs)
-            miny = min(ys)
             maxx = max(xs)
+            miny = min(ys)
             maxy = max(ys)
-            biggest_pore_group_dims = (max(biggest_pore_group_dims[0], maxx - minx),
-                                       max(biggest_pore_group_dims[1], maxy - miny))
+            w = maxx - minx
+            h = maxy - miny
+            biggest_pore_group_dims = (max(biggest_pore_group_dims[0], w),
+                                       max(biggest_pore_group_dims[1], h))
     biggest_pore_group_dims = (biggest_pore_group_dims[0]+20, biggest_pore_group_dims[1]+20)
     return biggest_pore_group_dims
 
 biggest_pore_group_dims = find_largest_pore_group_size()
 
-font_height = 15
+font_height = 20
+subset_font_height = 15
 font = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", font_height)
+subset_font = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", subset_font_height)
 
 
-def generate_cropped_image(combined_image, x_off=0, y_off=0, text=''):
-    subset_image = Image.new("1", (biggest_pore_group_dims[0] * len(subsets),
-                                   biggest_pore_group_dims[1] * len(subsets[0])), color=1)
+def generate_cropped_image(combined_image, x_off=0, y_off=0, text='', overlap_data=None):
+    per_group_text_padding = 75
+    sub_h = biggest_pore_group_dims[0]
+    sub_w = biggest_pore_group_dims[1]
+    subset_image = Image.new("1", (sub_w * len(subsets),
+                                   (sub_h + per_group_text_padding) * len(subsets[0])),
+                             color=1)
+    draw = ImageDraw.Draw(subset_image)
     for i, col in enumerate(subsets):
+        for j, row in enumerate(col):
+            overlap_percentage, xs, ys = overlap_data[i][j]
+            # xs = [x for x, y in row]
+            # ys = [y for x, y in row]
+            minx = min(xs)
+            miny = min(ys)
+            maxx = max(xs)
+            maxy = max(ys)
+            cropped_example = combined_image.crop((minx+x_off, miny+y_off, maxx+x_off, maxy+y_off))
+            topleft_x = sub_w * i
+            topleft_y = (sub_h+per_group_text_padding) * j
+            # if j:
+            #     topleft_y+=
+            subset_image.paste(cropped_example, (topleft_x, topleft_y))
+            draw.rectangle(((topleft_x, topleft_y), (topleft_x+sub_w, topleft_y+sub_h)), fill=None, outline=0)
+            draw.text((topleft_x, topleft_y + sub_h), '{:.2f}%'.format(overlap_percentage), 0, font=subset_font)
+    if text:
+        draw.text((0, subset_image.height-font_height), text, 0, font=font)
+    return subset_image
+
+
+def calc_overlap(slits, pores, x_off=0, y_off=0):
+    overlaps = []
+    for i, col in enumerate(subsets):
+        overlaps.append([])
         for j, row in enumerate(col):
             xs = [x for x, y in row]
             ys = [y for x, y in row]
@@ -137,38 +185,45 @@ def generate_cropped_image(combined_image, x_off=0, y_off=0, text=''):
             miny = min(ys)
             maxx = max(xs)
             maxy = max(ys)
-            cropped_example = combined_image.crop((minx+x_off, miny+y_off, maxx+x_off, maxy+y_off))
-            topleft_x = biggest_pore_group_dims[0]*i
-            topleft_y = biggest_pore_group_dims[1]*j
-            subset_image.paste(cropped_example, (topleft_x, topleft_y))
-    if text:
-        draw = ImageDraw.Draw(subset_image)
-        draw.text((0, subset_image.height-font_height), text, 0, font=font)
-    return subset_image
+            cropped_slit = slits.crop((minx + x_off, miny + y_off, maxx + x_off, maxy + y_off))
+            cropped_pores = pores.crop((minx, miny, maxx, maxy))
+            cropped_slit_np = np.asarray(cropped_slit)
+            cropped_pores_np = np.asarray(cropped_pores)
+            overlap_np = cropped_slit_np != cropped_pores_np
+            percent_overlap = 0.0
+            if np.sum(cropped_pores_np):
+                percent_overlap = (np.sum(overlap_np) / float(np.sum(cropped_pores_np))) * 100
+            else:
+                print('no pores??? seems like a bug')
+            print('percent_overlap {:.2f}'.format(percent_overlap))
+            overlaps[i].append((percent_overlap, xs, ys))
+            #Image.fromarray
+            pass
+    print('\n')
+    return overlaps
 
-images = [generate_cropped_image(combined, text='0 deg')]
-images[0].save('cropped0.png')
 
 def do_rotate(degree):
     ret = l4_output.rotate(degree, expand=True, fillcolor=1)
     rot_x = (ret.size[0]//2) - (l2_output.size[0]//2)
     rot_y = (ret.size[1]//2) - (l2_output.size[1]//2)
+    overlap_data = calc_overlap(slits=ret, pores=l2_output, x_off=rot_x, y_off=rot_y)
     # l2_resized = Image.new("1", (ret.width, ret.height), color=1)
     # l2_resized.paste(l2_output, (rot_x, rot_y))# ImageOps.invert(l2_output.convert('L')).convert('1'))
     # l2_resized.save('l2_resized.png')
-    # l2_mask = ImageOps.invert(l2_resized.convert('L', dither=None)).convert('1', dither=None)
     # ret.paste(l2_resized, (0,0), l2_mask)
-    #ret.paste(l2_output, (rot_x, rot_y), l2_mask)
-    add_pores(ret, rot_x, rot_y)
-    return ret, rot_x, rot_y
+    ret.paste(l2_output, (rot_x, rot_y), l2_mask)
+    #add_pores(ret, rot_x, rot_y)
+    return ret, rot_x, rot_y, overlap_data
 
-
-# rot, rot_x, rot_y = do_rotate(45)
-# rot.save('l4_rot_add_l2.png')
-for i in range(1, 30):
+images = []
+plus_minu_degree_range = 30
+degree_list = range(-plus_minu_degree_range, plus_minu_degree_range) + range(plus_minu_degree_range, -plus_minu_degree_range-1, -1)
+for i in degree_list:
     i=i / 10.
-    rot, rot_x, rot_y = do_rotate(i)
-    images.append(generate_cropped_image(rot, rot_x, rot_y, text='{} deg'.format(i)))
+    rot, rot_x, rot_y, overlap_data = do_rotate(i)
+    # rot.save('rot_{}.png'.format(i))
+    images.append(generate_cropped_image(rot, rot_x, rot_y, text='{} deg'.format(i), overlap_data=overlap_data))
     images[-1].save('cropped{}.png'.format(i))
     print('just saved cropped{}.png'.format(i))
 